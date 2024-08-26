7254ecaff1c31ecd41df016c89cf0e9db43a1307
Noga Avraham <noga001@gmail.com>
2024-08-26 13:40:56 +0300

    Make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -65,3 +65,15 @@ export function removeFromCart(productId) {
   //   }
   // });
 }
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (cartItem.productId === productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+  saveToStorage();
+}

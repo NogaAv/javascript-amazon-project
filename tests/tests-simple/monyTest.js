dcbdc7b6ef85b5a86f6f5c9f53058f71eaf695ef
Noga Avraham <noga001@gmail.com>
2024-08-27 15:04:31 +0300

    Rename tests-jasmine folder to tests

--- tests/tests-simple/monyTest.js
+++ tests/tests-simple/monyTest.js
@@ -1,4 +1,4 @@
-import formatCurrency from '../scripts/utils/money.js';
+import formatCurrency from '../../scripts/utils/money.js';
 
 //We should group related tests:
 console.log('-- test suite: formatCurrency --');
